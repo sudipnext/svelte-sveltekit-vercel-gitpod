aa594caa2113b90a579df2e504f9ad2aed8c0a28
sudipnext <parajulisudip123@outlook.com>
2022-01-24 11:00:28 +0545

    Css added

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,12 +2,47 @@
 	import TodoItem from '$lib/todo-item.svelte';
     let title = "TODOS | New";
 </script>
+<style>
+	.todos{
+	/* display: flex;
+	flex-direction: column;
+	flex-wrap: column wrap;
+	align-items: center; */
+	width: 100%;
+	max-width: 42rem;
+	margin: 4rem auto 0 auto;
+
+	}
+	.new{
+		margin: 0 0 0.5rem 0;
+	}
+	.new input{
+		font-size: 28px;
+		width: 100%;
+		padding: 0.5rem 1em .3em 1em;
+		opacity: 0.7;
+		box-sizing: border-box;
+		background: black;
+		border-radius: 8px;
+		text-align: center;
+	}
+	.todos :global(input){
+		border: 1px solid transparent;
+	}
+	.todos :global(input:focus-visible){
+		box-shadow: inset 1px 1px 6px rgba(0, 0, 0, 0.1);
+		border: 5px solid rgb(111, 0, 255);
+		outline: none;
+	}
+	
+</style>
 <svelte:head>
     <title>{title}</title>
 </svelte:head>
+<div class="todos">
 <h1>{title}</h1>
-<div class="form-input">
-	<form action="" method="">
+
+	<form action="" method="" class="new">
 		<input type="text" name="text" area-label="Add a todo" placeholder="+ Tap to add todo" />
 	</form>
 	<TodoItem />
@@ -15,12 +50,3 @@
 	<TodoItem />
 </div>
 
-<style>
-	.form-input,
-	h1 {
-		display: flex;
-		flex-direction: column;
-		flex-wrap: column wrap;
-		align-items: center;
-	}
-</style>
