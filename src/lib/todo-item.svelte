21d4ca4600148c91caf3527e62f8fe2fc122e89f
sudipnext <parajulisudip123@outlook.com>
2022-01-24 09:30:56 +0545

    items created

--- src/lib/todo-item.svelte
+++ src/lib/todo-item.svelte
@@ -1,5 +1,13 @@
 <div>
     <form action="" method="">
-        <input type="hidden" name="done" value="">
+        <input type="hidden" name="done" value=""/>
+        <button aria-label="mark done/not done">DONE/NOTDONE</button>
+    </form>
+    <form action="" method="">
+        <input type="text" />
+        <button aria-label="save todo">Save</button>
+    </form>
+    <form action="" method="">
+        <button aria-label="to delete a todo">Delete</button>
     </form>
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,23 @@
+<script>
+	import TodoItem from '$lib/todo-item.svelte';
+</script>
+
 <h1>TODOS</h1>
 <div class="form-input">
-<form action="" method="">
-    <input type="text" name="text" area-label="Add a todo" placeholder="+ Tap to add todo">
-</form>
-    </div>
-
+	<form action="" method="">
+		<input type="text" name="text" area-label="Add a todo" placeholder="+ Tap to add todo" />
+	</form>
+	<TodoItem />
+	<TodoItem />
+	<TodoItem />
+</div>
 
 <style>
-    .form-input, h1{
-        display: flex;
-        flex-direction: row;
-        flex-wrap: row wrap;
-        justify-content: center;
-    }
-   
+	.form-input,
+	h1 {
+		display: flex;
+		flex-direction: column;
+		flex-wrap: column wrap;
+		align-items: center;
+	}
 </style>
